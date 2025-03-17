4e4733de71a6ee29a7c5340ba495087bf31bc08e
hussain1442 <husssain1442@gmail.com>
2025-03-17 22:16:30 +0500

    Update config.js

--- config.js
+++ config.js
@@ -22,8 +22,8 @@ global.APIKeys = {
 }
 
 //═══════[modification]════════\\
-global.owner = ['918590565968','919446652312'] //owner number, u can put multiple owner number, within quotations and seperated by comma.
-global.pemilik = ['918590565968'] //another owner number
+global.owner = ['923317110234','923317110234'] //owner number, u can put multiple owner number, within quotations and seperated by comma.
+global.pemilik = ['923317110234'] //another owner number
 global.premium = ['918590565968'] //premium number
 global.pengguna = 'prince' //username
 global.botnma = 'MD-prince' //bot name
